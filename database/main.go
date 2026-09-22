bac9ead5b2abfca244a465880638188605efc657
normiecoder01 <normiecoder@gmail.com>
2026-09-22 09:29:53 +0530

    refactor database schema and user management functions; add user fetching and preparation statements

--- database/main.go
+++ database/main.go
@@ -1,53 +1,148 @@
 package main
 
 import (
-	"fmt"
 	"database/sql"
-	// This is unnamed import becasuse this import functionality 
+	"encoding/json"
+	"fmt"
+	"time"
+
+	// This is unnamed import becasuse this import functionality
 	// is going to be handled by the "database/sql" import
-	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"os"
-	"golang.org/x/crypto/bcrypt"
 
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var schema =`
+type User struct {
+	ID             int       `json:"id"`
+	Name           string    `json:"name"`
+	Email          string    `json:"email"`
+	HashedPassword string    `json:"-"`
+	CreatedAt      time.Time `json:"created_at"`
+}
+
+type UserWithoutPassword struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var schema = `
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT NOT NULL,
 email TEXT NOT NULL UNIQUE,
 hashedPassword BLOB NOT NULL,
-created at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 `
-func createUser(db *sql.DB, name , email , hashedPassword string ) (int64 , error) {
+
+func createSchema(db *sql.DB) {
+	_, err := db.Exec(schema)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Database table was created successfully!!")
+}
+func createUser(db *sql.DB, name, email, hashedPassword string) (int64, error) {
+	// Writing 'VALUES (? , ? , ?)' instead of adding the variables using printf is very crucial to avoid SQL Injection.
 	stmt := `INSERT INTO users (name , email , hashedPassword) VALUES (? , ? , ?)`
 
-	hp , err := bcrypt.GenerateFromPassword([]byte(hashedPassword) , bcrypt.DefaultCost )
-	if err!=nil{
+	hp, err := bcrypt.GenerateFromPassword([]byte(hashedPassword), bcrypt.DefaultCost)
+	if err != nil {
 		log.Fatal(err)
-		return 0 ,err
+		return 0, err
 	}
 
-	result , err := db.Exec(stmt , "Anupal", "anumail@gmail.com",string(hp))
-	if err!=nil{
+	result, err := db.Exec(stmt, name, email, string(hp))
+	if err != nil {
 		log.Fatal(err)
-		return 0 , err
+		return 0, err
 	}
 
-	return	result.LastInsertId()
+	return result.LastInsertId()
 
 }
-func main(){
-	// Here we are removing the file generated when the 
-	// program was previously run because we want to start 
+
+func createUserWithPrepStmt(db *sql.DB, name, email, hashedPassword string) (int64, error) {
+	// Replace the string with db.Prepare(<string>) and save into a variable called stmt
+	stmt, err :=db.Prepare(`INSERT INTO users (name , email , hashedPassword) VALUES (? , ? , ?)`)
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}	
+
+	hp, err := bcrypt.GenerateFromPassword([]byte(hashedPassword), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}
+
+	result, err := stmt.Exec(name, email, string(hp))
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}
+
+	return result.LastInsertId()
+
+}
+func fetchUserByEmail(db *sql.DB, email string) (*User, error) {
+	stmt := `SELECT id , name,  email , hashedPassword, created_at FROM users WHERE email = ?`
+
+	row := db.QueryRow(stmt, email)
+
+	var user User
+
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.HashedPassword, &user.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+func fetchUsers(db *sql.DB) ([]UserWithoutPassword, error) {
+
+	stmt := `SELECT id, name, email, created_at FROM users`
+
+	rows, err := db.Query(stmt)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []UserWithoutPassword
+	var user UserWithoutPassword		
+	for rows.Next() {
+		
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+
+}
+
+func main() {
+	// Here we are removing the file generated when the
+	// program was previously run because we want to start
 	// afresh with every new run
 	dbName := "data.db"
 	_ = os.Remove(dbName)
 
-	db , error := sql.Open("sqlite3", dbName)
-	if error != nil{
+	db, error := sql.Open("sqlite3", dbName)
+	if error != nil {
 		log.Fatal(error)
 	}
 	defer func() {
@@ -64,11 +159,43 @@ func main(){
 
 	fmt.Println("Database connection eastablished successfully!!")
 
-	_ , err := db.Exec(schema)
+	createSchema(db)
+
+	result, error := createUser(db, "Anupal Umale", "normiecoder@gmail.com", "Anupal@123")
+	error = db.Ping()
+	if error != nil {
+		log.Fatal(error)
+	}
+	fmt.Println(result)
+
+	result, error = createUserWithPrepStmt(db, "Ashutosh Paliwal", "ashupal@gmail.com", "ashupal@123")
+	error = db.Ping()
+	if error != nil {
+		log.Fatal(error)
+	}
+	fmt.Println(result)
+
+	anupal, err := fetchUserByEmail(db, "normiecoder@gmail.com")
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-		fmt.Println("Database table was created successfully!!")
+	fmt.Println(anupal)
+
+	bs, err := json.MarshalIndent(anupal, "", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bs))
+
+	users, err := fetchUsers(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bs, err = json.MarshalIndent(users, "", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(bs))
 
 }
